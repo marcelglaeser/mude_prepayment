9717ba11d3d1f6b4f7b7830946e878009ed2e255
marcelglaeser <marcel@holidaysgermany.de>
2014-12-20 01:20:28 +0100

    Update metadata.php

--- copy_this/modules/mudeprepayment/metadata.php
+++ copy_this/modules/mudeprepayment/metadata.php
@@ -1,33 +1,34 @@
 <?php
+
 /**
-* Changelog:
-* 19.11.2013 - eComStyle.de | Josef Andreas Puckl - Alle Dateien im Modulordner, Kompatibilit�t zu Shopversion 4.7 und 4.8, optische und inhaltliche Anpassungen.
-* Module information
-*/
+ * Changelog:
+ * 19.11.2013 - eComStyle.de | Josef Andreas Puckl - Alle Dateien im Modulordner, Kompatibilit�t zu Shopversion 4.7 und 4.8, optische und inhaltliche Anpassungen.
+ * 19.12.2014 - PlüschStore.de | Marcel Gläser - Das Modul läuft nun mit 4.9 und verwendet Blocks innerhalb der originalen order_list.tpl
+ * Module information
+ */
 $aModule = array(
-    'id'           => 'mudeprepay',
-    'title'        => 'Vorkasseerinnerung',
-    'description'  => 'Senden Sie mit nur einen Mausklick eine Vorkasseerinnerung',
-    'version'      => '2.0',
-    'thumbnail'    => '',
-    'author'       => 'OXID Community',
-    'email'        => '',
-    'url'          => '',
-	'extend'       => array(
-	),
+    'id' => 'mudeprepay',
+    'title' => 'Vorkasseerinnerung',
+    'description' => 'Senden Sie mit nur einen Mausklick eine Vorkasseerinnerung',
+    'version' => '3.0',
+    'thumbnail' => '',
+    'author' => 'OXID Community',
+    'email' => '',
+    'url' => '',
+    'extend' => array(
+        'order_list' => 'mudeprepayment/admin/mude_prepayment_order_list',
+    ),
     'templates' => array(
-        'mude_prepayment_order.tpl'           => 'mudeprepayment/out/admin/tpl/mude_prepayment_order.tpl',
-        'mude_prepayment_order_list.tpl'      => 'mudeprepayment/out/admin/tpl/mude_prepayment_order_list.tpl',
-        'email_mudeprepayment_customer.tpl'   => 'mudeprepayment/out/azure/tpl/email_mudeprepayment_customer.tpl',           
+        'email_mudeprepayment_customer.tpl' => 'mudeprepayment/views/azure/tpl/email_mudeprepayment_customer.tpl',
     ),
     'files' => array(
-        'mude_prepayment_admin_order'         => 'mudeprepayment/admin/mude_prepayment_admin_order.php',
-        'mude_prepayment_order_list'          => 'mudeprepayment/admin/mude_prepayment_order_list.php',
+        'mude_prepayment_order_list' => 'mudeprepayment/admin/mude_prepayment_order_list.php',
     ),
     'blocks' => array(
-           
- ), 
-     'settings' => array(
-
+        array('template' => 'order_list.tpl', 'block' => 'admin_order_list_filter', 'file' => '/views/blocks/mude_prepayment_order_list_filter.tpl'),
+        array('template' => 'order_list.tpl', 'block' => 'admin_order_list_sorting', 'file' => '/views/blocks/mude_prepayment_order_list_sorting.tpl'),
+        array('template' => 'order_list.tpl', 'block' => 'admin_order_list_item', 'file' => '/views/blocks/mude_prepayment_order_list_item.tpl'),
+    ),
+    'settings' => array(
     ),
 );
